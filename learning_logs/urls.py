5257f6bf7f2a04732020ce04cfc71ec92d693357
zjz <sc2nino@126.com>
2018-07-13 11:06:14 +0800

    topic done

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^$',views.index, name= 'index')
+        url(r'^$',views.index, name= 'index'),
+        url(r'^topics/$',views.topics,name='topics'),
+        #特定主题的详细页面
+        url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
 ]
